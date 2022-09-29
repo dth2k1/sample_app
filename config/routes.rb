5643118336ff898a9320a205f32febb6bd90cb12
dth2k1 <gblhiep@gmail.com>
2022-09-29 15:51:12 +0700

    add static file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'stactic_pages/home'
+  get 'stactic_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
